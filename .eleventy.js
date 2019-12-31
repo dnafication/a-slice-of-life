062370672f119a44990320e7c11dd01deb5fd705
dnafication <dnafication@users.noreply.github.com>
2019-12-31 20:54:39 +1100

    added markdown support and fetch

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,29 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
+  let markdownIt = require('markdown-it');
+  let markdownItAnchor = require('markdown-it-anchor');
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+  let opts = {
+    permalink: true,
+    permalinkClass: 'direct-link',
+    permalinkSymbol: '#'
+  };
+
+  eleventyConfig.setLibrary(
+    'md',
+    markdownIt(options).use(markdownItAnchor, opts)
+  );
+
+  eleventyConfig.addFilter('markdownify', function(value) {
+    const md = new markdownIt(options);
+    return md.render(value);
+  });
+
   return {
     passthroughFileCopy: true,
     markdownTemplateEngine: 'njk',
