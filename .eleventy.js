699ab4569638e6f518722ec7de18081bdaef1855
dnafication <dnafication@users.noreply.github.com>
2019-12-31 15:25:26 +1100

    navigation fixed
    - removed unwanted pages
    - removed newsletter
    - added order to pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/css');
   eleventyConfig.addPassthroughCopy('src/fonts');
   eleventyConfig.addPassthroughCopy('src/js');
   eleventyConfig.addPassthroughCopy('src/images');
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   return {
     passthroughFileCopy: true,
